d2bc747915fc17e304050e1abd28b2f732305b7b
laulv <levanlau.net@gmail.com>
2025-06-14 15:08:51 +0700

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest
+terraform {
+  required_version = ">=1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.40"
+    }
+  }
+}
+
+# =============================
+
 provider "aws" {
   region = var.region
 }
